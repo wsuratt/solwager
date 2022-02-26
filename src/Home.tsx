7a96f96b3d7bcdff64c865cabd0bab9da73b815b
William Suratt <wsuratt@comcast.net>
2022-02-26 01:34:42 -0800

    add description

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,16 +15,33 @@ const Home = () => {
     <main className={classes.main}>
       <Typography className={classes.title}>SOL</Typography>
       <Typography className={classes.title2}>WAGER</Typography>
-      <Typography className={classes.text}>coming soon...</Typography>
+      <div className={classes.center}>
+        <Box sx={{ width: 500, marginTop: 5, p: 2, border: '1px solid white' }}>
+          <Typography className={classes.text}>Skill-based game wagering platform built on Solana</Typography>
+        </Box>
+      </div>
+      <Typography className={classes.text2}>coming soon...</Typography>
     </main>
   )
 };
 const useStyles = makeStyles({
-  main : {
+  center: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  main: {
     textAlign: "center",
     background: "black",
   },
   text: {
+    color: "white",
+    fontSize: "25px",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "Verdana",
+  },
+  text2: {
     color: "white",
     marginTop: "25px",
     fontSize: "15px",
